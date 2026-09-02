f1a0550c46ac46ab4e6505c1038d70b483fe7fbe
todzyto <josepimenta545@gmail.com>
2026-09-01 22:51:32 -0300

    exclui os sprites antigos

--- scripts/musica.gd
+++ scripts/musica.gd
@@ -5,6 +5,6 @@ onready var tween = $Tween
 func _ready():
 	volume_db = -30
 	play()
-	tween.interpolate_property(self, "volume_db", -30, 0, 2.0,Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	tween.interpolate_property(self, "volume_db", -30, -17, 2.0,Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	
 	tween.start()
